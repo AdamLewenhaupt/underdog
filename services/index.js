59a332226baf95e400a25c02b9ff0559313a3f8c
Adam Lewenhaupt <adam.lewenhauptt@gmail.com>
2013-03-19 11:02:30 +0100

    made coffee

--- services/index.js
+++
@@ -1,9 +0,0 @@
-/*
-The services package provides a interface to all services,
-such as algorithms, chatting, broadcasting etc.
-*/
-
-exports.services = {
-	io: require('./io'),
-	auth: require('./auth')
-}
