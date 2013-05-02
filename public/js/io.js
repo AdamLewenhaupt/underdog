fa66a2183cac62ead7fda1d469079c88e63f8533
Adam Lewenhaupt <adam.lewenhauptt@gmail.com>
2013-05-02 20:54:35 +0200

    ready for h

--- public/js/io.js
+++ public/js/io.js
@@ -11,11 +11,11 @@ define([], function(){
 
 	return {
 		initialize: function() {
-			/*socket = io.connect('http://beviewed.herokuapp.com/', {
+			socket = io.connect('http://beviewed.herokuapp.com/', {
 				transports: ["xhr-polling"]
-			});*/
+			});
 
-			socket = io.connect("http://localhost:3000/");
+			//socket = io.connect("http://localhost:3000/");
 
 			inited = true;
 			init.forEach(function(fn){
