22cd0c0121276e54c30fb73b48eb19ac8fb2ad58
Adam Lewenhaupt <adam.lewenhauptt@gmail.com>
2013-05-08 17:48:25 +0200

    more work done on notifications

--- public/js/ufronts/notification-center.js
+++ public/js/ufronts/notification-center.js
@@ -1,5 +1,5 @@
-define(["ufront/ufront", "priority-queue"], 
-	function (UFront, PriorityQueue){
+define(["ufront/ufront", "priority-queue", "jquery-ui"], 
+	function (UFront, PriorityQueue, $ui){
 
 	var NotificationCenter = new UFront({
 
@@ -10,7 +10,9 @@ define(["ufront/ufront", "priority-queue"],
 
 			defaults: {
 
-				queue: new PriorityQueue({ low: true })
+				queue: new PriorityQueue({ low: true }),
+				notifying: false,
+				current: false
 			},
 
 			pushable: {
@@ -30,8 +32,79 @@ define(["ufront/ufront", "priority-queue"],
 			},
 
 			rendable: {
-				template: "<div>Test</div>"
+				template: "<div class='note' style='<%= current.css %>position:relative;width:100%;height:100%;'><%= current.html %></div>",
+				triggers: ["current"],
+
+				onRend: function (view){
+
+					var $note = view.$el.find(".note"),
+						color = $note.css("background-color"),
+						highlight = "#FFFFFF";
+
+					$note.animate({
+
+						backgroundColor: highlight
+
+					}, 400, function (){
+
+						$note.animate({
+							backgroundColor: color
+						}, 50);
+					});
+				}
 			}
+		},
+
+		extend: function (main){
+
+			main.onInit('model', function (model){
+
+				model.on('change:queue', function (){
+
+					if(!model.get("notifying")) {
+
+						model.set("notifying", true);
+						model.show(model.get("queue").pop());
+					}
+				});
+
+				model.get("queue").push({
+
+					html: "This is a test notification",
+					css: { color: "white", "background-color": "#4DBF4D" },
+					duration: 10
+
+				}, 1);
+
+				model.trigger("change:queue");
+
+			});
+
+			main.Model.show = function (note){
+
+				var model = this;
+
+				if(note.duration){
+
+					setTimeout(function (){
+
+						model.trigger("next");
+						
+					}, note.duration);
+				}
+
+				var css = "";
+
+				for(attr in note.css)
+					css += (attr + ":" + note.css[attr] + ";")
+
+				model.set("current", {
+
+					html: note.html,
+					css: css
+				});
+
+			};
 		}
 
 	}).Create;
